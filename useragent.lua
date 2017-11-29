92e48e77e0a5c61ee184f57661f19431777f50e9
hemincan <hemincan@github.com>
2017-11-29 06:57:11 -0800

    disconnect

--- useragent.lua
+++ useragent.lua
@@ -17,6 +17,8 @@ local message = {}
 local myroom
 local name
 local web_socket --websocket
+local unreceiveheatbeatcount = 0
+local disconnect = false
 skynet.start(function()
 	skynet.dispatch("lua", function(session, source, cmd, ...)
 		if CMD[cmd] then
@@ -80,17 +82,7 @@ end
 
 ---------------------
 ---------------------
-function CMD.toClient( data )
-	local success,jsonstr = pcall(json.encode,data)
-	if success then
-		if _type=="socket" then
-			socket.write(fd,jsonstr.."\n")
-		end
-		if _type=="websocket" then
-			web_socket:send_text(jsonstr)
-		end
-	end
-end
+
 function receiveMessage_websocket( )
 	fd=tonumber(fd)
 	socket.start(fd)
@@ -127,6 +119,23 @@ function handleMessage( str )
 		end
 	end
 end
+
+function beginHeatBeat(  )
+	skynet.fork(function (  )
+		while true do
+			skynet.error("status",disconnect,unreceiveheatbeatcount)
+			unreceiveheatbeatcount=unreceiveheatbeatcount+1
+			if unreceiveheatbeatcount>3 then
+				disconnect=true
+			end
+			if unreceiveheatbeatcount>=100 then
+				skynet.send("USERDATA","lua","removeUser",name)
+				exitService()
+			end
+			skynet.sleep(1000)
+		end
+	end)
+end
 function CMD.joinSuccess( room )
 	myroom=room
 	skynet.error("joinSuccess")
@@ -135,30 +144,63 @@ function CMD.joinSuccess( room )
 	local success = skynet.call(myroom,"lua","addPlayer",name,skynet.self())
 	CMD.toClient(data)
 end
+function CMD.toClient( data )
+	local success,jsonstr = pcall(json.encode,data)
+	if success then
+		if _type=="socket" then
+			socket.write(fd,jsonstr.."\n")
+		end
+		if _type=="websocket" then
+			web_socket:send_text(jsonstr)
+		end
+	end
+end
+function CMD.oldData( oldData )
+	message=oldData.message
+	myroom=oldData.room
+end
+function CMD.reconnect( newagent )
+	--data need to give the newagent
+	skynet.send(newagent,"lua","oldData",{message=message,room=myroom})
+	exitService()
+end
+function exitService(  )
+	skynet.exit()
+end
 function Action.login( data )
    
     name = data.name
-	skynet.send("USERDATA","lua","addUser",name,skynet.self())
+
+    local user = skynet.call("USERDATA","lua","getUser",name)
+    if user==nil then
+		skynet.send("USERDATA","lua","addUser",name,skynet.self())
+	else
+		skynet.send(user.agent,"lua","reconnect",skynet.self())
+		skynet.send("USERDATA","lua","setAgent",name,skynet.self())
+	end
+	beginHeatBeat()
 	-- local agent = skynet.call("USERDATA","lua","getUser",name)
 	-- skynet.error(agent)
 end
+function Action.heatBeat(  )
+	unreceiveheatbeatcount=0
+end
 function Action.logout( data )
 	skynet.error("logout")
-end
-function Action.reconnect( data )
-	skynet.error("reconnect")
 end
 function toAllPeople( data )
 	local agents = skynet.call("USERDATA","lua","getAllUser")
 	for k,v in pairs(agents) do
 		print(k,v)
-		skynet.send(v,"lua","toClient",data)
+		skynet.send(v.agent,"lua","toClient",data)
 	end
 end
 function Action.sendMessageTo( data )
     local name = data.name
-	local agent = skynet.call("USERDATA","lua","getUser",name)
-	skynet.send(agent,"lua","toClient",data)
+	local user = skynet.call("USERDATA","lua","getUser",name)
+	if user~=nil then
+		skynet.send(user.agent,"lua","toClient",data)
+	end
 end
 function Action.newGame( data )
 	skynet.send("ROOMMANAGER","lua","newGame",skynet.self(),name)
--- userdata.lua
+++ userdata.lua
@@ -4,13 +4,26 @@ local data = {}
 local CMD = {}
 function CMD.addUser(name,agent) 
 	skynet.error("addUser!")
-	data[name]=agent
+	data[name]={agent=agent}
 end
 function CMD.getUser( name )
 	return data[name]
 end
 function CMD.getAllUser(  )
 	return data
+end
+function CMD.removeUser(name)
+	skynet.error("removeUser",name)
+	data[name]=nil
+end
+function CMD.setAgent( name,agent )
+	if data[name] then
+		data[name].agent=agent
+	else
+		skynet.error("userdata noexit this user",name)
+	end
+	
+
 end
 skynet.start(function()
 	skynet.dispatch("lua", function(session, source, cmd, ...)
